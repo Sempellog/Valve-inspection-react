60a2e393af42ea191e493e631e2cd54c6d658d56
sempellog <harrischung85@gmail.com>
2026-06-07 00:04:01 +0800

    feat: add checklist component

---
+++ src/components/checklistItem.jsx
@@ -0,0 +1,15 @@
+function ChecklistItem({item, value, onChange}) {
+    return (
+        <div className="bg-white rounded-lg p-4 shadow-sm flex flex-cols md:flex-row md:items-center justify-between gap-4">
+            <p className="text-gray-700 text-sm flex-1">{item.label}</p>
+            <select value={value} onChange={(e) => onChange(item.id, e.target.value)} className="border border-gray-300 rounded px-3 bg-white">
+                <option value="">Select</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+                <option value="na">N/A</option>
+            </select>
+        </div>
+    )
+}
+
+export default ChecklistItem;
